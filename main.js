036cf9931adeabf37130503bed2aaa0aae173bed
Philip Edekobi <philipedekobi@gmail.com>
2024-06-03 08:54:16 +0100

    doneE

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ import * as THREE from "three";
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 
-const shadGroup = new THREE.Group();
+
+const video = document.getElementById("vid")
+
+const raycaster = new THREE.Raycaster()
+const mouse = new THREE.Vector2()
 
 const el = document.getElementById("canv")
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true, canvas: el });
@@ -13,9 +17,13 @@ const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerH
 camera.position.set(0, 0, 25);
 camera.lookAt(0, 0, 0);
 
+let width = 0
+let height = 0
+let intersects = []
+
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(8, 2, 2, 8, 4, 6)
+const geometry = new THREE.BoxGeometry(8, 2, 2)
 
 const edgesGeometry = new THREE.EdgesGeometry(geometry)
 const material = new THREE.LineDashedMaterial({ color: 0x2377fc, dashSize: 4 })
@@ -24,15 +32,14 @@ const edges = new THREE.LineSegments(edgesGeometry, material)
 edges.position.y = 8.5
 // edges.rotation.x = Math.PI * 0.1
 // edges.rotation.y = Math.PI * 0.1
-edges.castShadow = true
-shadGroup.add(edges)
 
-scene.add(shadGroup)
-// scene.add(edges)
+scene.add(edges)
+
+let txt
 
 const textLoader = new FontLoader()
 textLoader.load("node_modules/three/examples/fonts/droid/droid_sans_regular.typeface.json", (font) => {
-    const textGeometry = new TextGeometry("Turn on TV", {
+    const textGeometry = new TextGeometry("Play Video", {
         font,
         size: 1,
         depth: 0,
@@ -46,21 +53,56 @@ textLoader.load("node_modules/three/examples/fonts/droid/droid_sans_regular.type
     // text.rotation.x = Math.PI * 0.2
     // text.rotation.y = Math.PI * 0.1
 
+    txt = text
     scene.add(text)
     renderer.render(scene, camera)
 })
-
-const dl = new THREE.DirectionalLight(0xFFFFFF, 0.5);
-dl.position.set(0, 2, 4)
-dl.castShadow = true;
-shadGroup.add(dl)
 
 renderer.render(scene, camera);
 
 function animate() {
     edges.rotation.x += 0.01
     // edges.rotation.y += 0.01
+
     renderer.render(scene, camera)
 }
 
 renderer.setAnimationLoop(animate)
+
+// events
+function resize() {
+    width = window.innerWidth
+    height = window.innerHeight
+    camera.aspect = width / height
+    const target = new THREE.Vector3(0, 0, 0)
+    const distance = camera.position.distanceTo(target)
+    const fov = (camera.fov * Math.PI) / 180
+    const viewportHeight = 2 * Math.tan(fov / 2) * distance
+    camera.updateProjectionMatrix()
+    renderer.setSize(width, height)
+}
+
+window.addEventListener('resize', resize)
+resize()
+
+
+window.addEventListener('mousemove', (e) => {
+    mouse.set((e.clientX / width) * 2 - 1, -(e.clientY / height) * 2 + 1)
+    raycaster.setFromCamera(mouse, camera)
+    intersects = raycaster.intersectObjects(scene.children)
+    const findCube = intersects.find((hit) => hit.object === edges)
+    if (findCube) { document.body.style.cursor = "pointer" } else {
+        document.body.style.cursor = "default"
+    }
+    edges.material.color.convertSRGBToLinear()
+})
+
+window.addEventListener("click", e => {
+    const findCube = intersects.find((hit) => hit.object === edges || hit.object === txt)
+    console.log(intersects)
+    console.log(findCube);
+
+    if (findCube) {
+        video.play()
+    }
+})
